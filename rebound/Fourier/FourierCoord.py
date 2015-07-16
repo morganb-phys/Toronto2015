ff5fd274a21435063021b4b9f22dbc277c6b8f68
morganb-phys <morganb.phys@gmail.com>
2015-07-16 16:22:42 -0400

    Fixed read from file

--- rebound/Fourier/FourierCoord.py
+++ rebound/Fourier/FourierCoord.py
@@ -28,7 +28,7 @@ def FindPeak(fourier,freq,dt,span=None):
     
     #print 'Threshold:', threshold
     freqSpace = freq[1]-freq[0]
-    
+
     if span==None:
         span = [freq.min(),freq.max()]
     
@@ -53,37 +53,49 @@ def FindPeak(fourier,freq,dt,span=None):
 
             # Mask pulse from future searching
             width = PeakWidth(fourier,freq,currentMax,
-                              currentMaxLoc,freqSpace,method='gaussian') 
- 
+                              currentMaxLoc,freqSpace) 
+
+            # Zero out that location so it won't find same pulse
             mask[width[0]:width[1]] = 0.  
  
         
         else:
+            # Plots the fourier transform for a single planet and the 
+            # masked array to make sure all have been removed
             plt.figure()
             plt.plot(freq,np.abs(fourier)*mask)
             plt.plot(freq,np.abs(fourier))
             plt.show()
             break
 
+    # Sorts the peaks from largest to smallest
     pulseList=sorted(pulseDict.keys(),key=lambda x: -pulseDict[x])
 
+    # return the number of the pulse, height of pulse and 
+    # frequency location of pulse
     return [(i,pulseDict[i],freq[i]) for i in pulseList]    
 
-    
+#Fitting functions to find width of peak
 def f(x,a,b,c):
     return a*np.exp(-(x-b)**2/(2.*c**2))
 
 def laplace(x,a,b,c):
     return a*np.exp(-np.abs(x-b)/c)
 
+# Find peak width
 def PeakWidth(fourier,freq,currentMax,currentMaxLoc,freqSpace,method=None):
-    
+
+    # General rule is that the width of the peak is
+    # height/5e2
     if method == 'FracHeight' or method == None:
         width = currentMax/5.e2
+        
+        # returns an array of the approximate location of peak
         return [int(max(0,math.floor(currentMaxLoc-width/freqSpace))),
                     int(math.ceil(currentMaxLoc+width/freqSpace))]
 
-    elif method == 'gaussian':
+    #fits to the peak and uses std dev to define width
+    elif method == 'fit':
  
         bins = PeakWidth(fourier,freq,currentMax,currentMaxLoc,freqSpace)
        
@@ -92,23 +104,17 @@ def PeakWidth(fourier,freq,currentMax,currentMaxLoc,freqSpace,method=None):
         freqmin = popt[1]-3.*popt[2]
         freqmax = popt[1]+3.*popt[2]
         
-        print popt
-        print freqmin
-        print freqmax
-
-        print currentMaxLoc
-        print  [max(0,math.floor(currentMaxLoc-freqmin/freqSpace)),
-                    math.ceil(currentMaxLoc+freqmax/freqSpace)]
+        # returns an array of the approximate location of peak
         return [max(0,math.floor(currentMaxLoc-freqmin/freqSpace)),
                     math.ceil(currentMaxLoc+freqmax/freqSpace)]
         
 
 def VarfromFile(Nout,NumP,tmax = None,FileName = None):
-    # Read the data from the x-axis
     
     if FileName == None:
         FileName = 'data/'+"{:.0E}".format(tmax)+'Yanqin_X.txt'
 
+    
     File = open(FileName,'r')
     NumVar = len(string.split(File.readline()))/NumP
 
@@ -148,7 +154,7 @@ def VarfromFile(Nout,NumP,tmax = None,FileName = None):
                             x[k][j][i] = float(word)
      
             File.close()
-            return time,x
+            return x,time
                             
         else:
             print 'Unable to read the data file.'
@@ -158,19 +164,22 @@ def VarfromFile(Nout,NumP,tmax = None,FileName = None):
 
 if __name__=="__main__":
 
-    Nout = 10001
-    tmax=1.e2
-    x = VarfromFile(Nout,6,tmax=tmax)[0]
+    Nout = 1001
+    tmax=5000.
+    
+    x = VarfromFile(Nout,6,tmax=tmax,FileName='data/dt5_eOo.txt')[0]
     time = np.linspace(0,tmax,Nout)
     dt = tmax/(Nout-1.)
     Planets = ['k11b','k11c','k11d','k11e','k11f','k11g']
     Resonances = {}
 
+    ecosPomega = x[0]*np.cos(x[1]+x[2])
 
-    ft= fft(x)
+
+    ft= fft(ecosPomega)
     freq = np.linspace(0.,1./(2.*dt), Nout/2)
 
-
+    '''
     for i,planet in enumerate(Planets):
         print i
         Resonances[planet] = FindPeak(ft[i][0:Nout/2],freq,dt)
@@ -180,9 +189,14 @@ if __name__=="__main__":
         print planet
         for frequency in Resonances[planet]:
             print frequency[2]
-         
+    '''
+     
+    itg.PlotLatex()
+
     plt.figure()
     plt.xlabel('Frequency $(yr^{-1})$')
+    plt.xlim([0.01,0.1])
+    plt.ylim([0,0.6])
     for i,planet in enumerate(Planets):
         plt.plot(freq,np.absolute(ft[i][0:Nout/2]),label=planet)
     plt.legend()
